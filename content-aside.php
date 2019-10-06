00bc6acdf652a7ec864df712b15b26aee2cee6b3
BaPpY <bappyasif@hotmail.com>
2019-10-07 02:18:32 +0600

    Added To Previous Commit With Post Formats Support For WP Theme Development

---
+++ content-aside.php
@@ -0,0 +1,9 @@
+<!-- Oh Hello -->
+
+<article class="post-aside">
+    
+    <p class="mini-meta"><?php the_author(); ?> @ <?php the_time('F j, Y'); ?></p>
+
+    <?php the_content(); ?>
+
+</article>

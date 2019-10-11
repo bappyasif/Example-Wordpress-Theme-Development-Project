a8e03e6fedc77ef504eb19ee33500c33b27c0f74
BaPpY <bappyasif@hotmail.com>
2019-10-12 03:02:37 +0600

    Added To Previous Commit With WP_QUERY Manifestations Which Also Includes Some CSS Stylinig

--- front-page.php
+++ front-page.php
@@ -21,6 +21,67 @@ get_header(); ?>
 
             endif; ?>
         
+        
+        <!-- home-columns -->
+        <div class="home-columns clearfix">
+            
+            <!-- one-half -->
+            <div class="one-half">
+                            
+                <?php //<h3>Custom HTML Goes Here</h3>
+                
+                // First Loop Content For Categories
+                $opinionPosts = new WP_Query('cat=1&posts_per_page=2');
+                // $opinionPosts = new WP_Query('cat=1&posts_per_page=2&orderby=title&order=ASC');
+                
+                if ( $opinionPosts->have_posts() ) :
+                    
+                    while ( $opinionPosts->have_posts() ) : $opinionPosts->the_post(); ?>
+                            
+                        <!-- Content output -->
+                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                        <?php the_excerpt(); ?>
+                
+                    <?php endwhile;
+                else :
+                // Fallback Content
+                
+                endif;
+                
+                // preventing Wp_Query to disrupt Wordpress natural uyrl based loops.
+                wp_reset_postdata(); ?>
+            
+            </div> <!-- /one-half -->
+            
+            <!-- second-half -->
+            <div class="one-half last">
+                
+                <?php // Seeccond Loop Content For Categories
+                $opinionPosts = new WP_Query('cat=6&posts_per_page=2');
+                // $opinionPosts = new WP_Query('cat=1&posts_per_page=2&orderby=title&order=ASC');
+                
+                if ( $opinionPosts->have_posts() ) :
+                    while ( $opinionPosts->have_posts() ) : $opinionPosts->the_post(); ?>
+                         
+                        <!-- Content output -->
+                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                        <?php the_excerpt(); ?>
+                
+                    <?php endwhile;
+                else :
+                // Fallback Content
+                
+                endif;
+                
+                // preventing Wp_Query to disrupt Wordpress natural uyrl based loops.
+                wp_reset_postdata();
+                
+                ?>
+            
+            </div> <!-- /one-half -->
+        
+        </div> <!-- /home-columns -->
+        
         <h3>Custom HTML Goes Here</h3>
 
     </div>

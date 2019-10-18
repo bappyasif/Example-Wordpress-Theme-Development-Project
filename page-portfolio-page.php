8b5fb593a9a8d40e1e846afef319d732c9356b7e
BaPpY <bappyasif@hotmail.com>
2019-10-19 02:54:39 +0600

    Added To Previous Commit With REST API Implementation For Portfolio Page Using Javascript & Json

---
+++ page-portfolio-page.php
@@ -0,0 +1,47 @@
+<?php
+
+get_header();
+
+if (have_posts()) :
+
+	while (have_posts()) : the_post(); ?>
+
+        <article class="post">
+            <!-- Collumn Container-->
+            <div class="collumn-continer clearfix">
+                
+                <!-- Title Collumn -->
+                <div class="title-collumn">
+                    
+                    <h2> <?php the_title(); ?> </h2>
+                    
+                </div> <!-- Title Collumn Tag Closing -->
+                
+                <!-- Text Collumn -->
+                <div class="text-collumn">
+                    
+                    <?php the_content(); ?>
+                    
+                    <button id="portfolio-posts-btn">Load Portfolio Related Blog Posts</button>
+                    <div id="portfolio-posts-container"></div>
+                
+                </div> <!-- Text Collumn Tag Closing -->
+            
+            </div> <!-- Collumn Container Tag Closing -->
+            
+                
+                
+
+        </article>
+
+	<?php endwhile;
+	
+	else:
+
+		echo '<p> <b> No Content Been Found!!! </b> </p>';
+
+	endif;
+
+get_footer();
+
+?>
